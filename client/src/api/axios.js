4566ca959ce0d452f4bfa2a3fa928feb42aba9c4
ellis-guo <sarki@foxmail.com>
2025-11-27 18:57:47 -0800

    Fix baseURL for Vercel

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create axios instance with default configuration
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // Backend API address
+  baseURL: "https://onlinestore-kzw0.onrender.com/api", // Backend API address
   withCredentials: true, // Include cookies in requests (for JWT authentication)
   headers: {
     "Content-Type": "application/json", // Send JSON data
